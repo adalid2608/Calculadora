cba001b3528d7f61e368a84f108290ab86d162fe
adalid2608 <adalid.islas26@gmail.com>
2024-07-02 20:40:13 -0600

    Actualización de Archivos para mostrar por medio de un TOAST un mensaje que define cada una de las etapas del Ciclo de Un Activity.

--- app/src/main/java/com/adalid/basicsapp9b/MainActivity.java
+++ app/src/main/java/com/adalid/basicsapp9b/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         txtNum1 = findViewById(R.id.txtNum1);
         txtNum2 = findViewById(R.id.txtNum2);
         txtResult = findViewById(R.id.txtResult);
@@ -86,5 +86,39 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Los Campos se Limpiaron", Toast.LENGTH_SHORT).show();
             return;
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
